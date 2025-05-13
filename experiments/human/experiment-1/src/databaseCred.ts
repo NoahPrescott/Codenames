8aa71d8634aa53868acaac0b793888fb48dcbe65
Noah <143467457+NoahPrescott@users.noreply.github.com>
2025-05-13 09:24:10 -0400

    Update databaseCred.ts

--- experiments/human/experiment-1/src/databaseCred.ts
+++ experiments/human/experiment-1/src/databaseCred.ts
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyAasDKPaPCt8mIv-tr-De59kezdwwAx_cM",
+  apiKey: "",
   authDomain: "judgement-comp.firebaseapp.com",
   projectId: "judgement-comp",
   storageBucket: "judgement-comp.appspot.com",
   messagingSenderId: "649265869521",
-  appId: "1:649265869521:web:578e9846f9b6cb48354bf0",
+  appId: "",
   measurementId: "G-7V2F67JKKV"
 };
